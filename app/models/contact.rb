8c0960e4043632f6922a10deb061e99b49c437f7
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-04-02 14:00:59 -0300

    crud with has_one

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,9 +1,9 @@
 class Contact < ApplicationRecord
   belongs_to :kind
-  has_many :phones
-  has_one :address
+  has_many :phones, dependent: :destroy
+  has_one :address, dependent: :destroy
   accepts_nested_attributes_for :phones, allow_destroy: true
-  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :address, update_only: true
 
   def author
     "Jackson Pires"
