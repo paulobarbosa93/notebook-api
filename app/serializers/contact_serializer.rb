2599c8254abf18b08008432074ad388f12042eaf
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-04-10 02:05:42 -0300

    ams and links (HATEOAS)

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:kind) { kind_url(object.kind) }
+  end
+
   has_many :phones
   has_one :address
+
+  link(:self) { contact_url(object) }
 
   meta do
     { author: "Paulo Barbosa" }
