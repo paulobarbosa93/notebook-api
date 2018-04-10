5ea3a2d616189162d5c1659c740cc296c50a6a66
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-04-10 01:18:28 -0300

    i18n + json_api adapter

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,14 +4,4 @@ class Contact < ApplicationRecord
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :phones, allow_destroy: true
   accepts_nested_attributes_for :address, update_only: true
-
-  def author
-    "Jackson Pires"
-  end
-
-  def as_json(options = {})
-    h = super(options)
-    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
-    h
-  end
 end
