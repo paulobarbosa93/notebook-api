58b7ee47b246f6f89b77a0dedaade5e184dde443
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-04-11 00:40:14 -0300

    refactoring

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,13 +2,13 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
   belongs_to :kind do
-    link(:kind) { kind_url(object.kind) }
+    link(:related) { kind_url(object.kind) }
   end
 
   has_many :phones
   has_one :address
 
-  link(:self) { contact_url(object) }
+  # link(:self) { contact_url(object) }
 
   meta do
     { author: "Paulo Barbosa" }
