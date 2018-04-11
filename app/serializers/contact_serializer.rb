472319e56212a442343b24de29fe1b1c9bcfe662
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-04-11 01:05:53 -0300

    media types

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -8,15 +8,12 @@ class ContactSerializer < ActiveModel::Serializer
   has_many :phones
   has_one :address
 
-  # link(:self) { contact_url(object) }
-
   meta do
     { author: "Paulo Barbosa" }
   end
 
   def attributes(*args)
     h = super(*args)
-    # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h
   end
