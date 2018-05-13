5de9840d7aab09b995cdc716f823fa389588c2a3
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-05-13 18:57:03 -0300

    versionist with http header

--- config/routes.rb
+++ config/routes.rb
@@ -5,33 +5,33 @@ Rails.application.routes.draw do
 
   resources :auths, only: [:create]
 
-  api_version(:module => "V1", parameter: {name: "version", value: "1"}) do
+  api_version(module: 'V1', header: { name: 'Accept', value: 'application/vnd.api+json; version=1' }) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
 
       resource :phones, only: [:show]
       resource :phones, only: [:show], path: 'relationships/phones'
-      resource :phone, only: [:update, :create, :destroy]
-      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+      resource :phone, only: %i[update create destroy]
+      resource :phone, only: %i[update create destroy], path: 'relationships/phones'
 
-      resource :address, only: [:show, :update, :create, :destroy]
-      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      resource :address, only: %i[show update create destroy]
+      resource :address, only: %i[show update create destroy], path: 'relationships/address'
     end
   end
 
-  api_version(:module => "V2", parameter: {name: "version", value: "2"}) do
+  api_version(module: 'V2', header: { name: 'Accept', value: 'application/vnd.api+json; version=2' }) do
     resources :contacts do
       resource :kind, only: [:show]
       resource :kind, only: [:show], path: 'relationships/kind'
 
       resource :phones, only: [:show]
       resource :phones, only: [:show], path: 'relationships/phones'
-      resource :phone, only: [:update, :create, :destroy]
-      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+      resource :phone, only: %i[update create destroy]
+      resource :phone, only: %i[update create destroy], path: 'relationships/phones'
 
-      resource :address, only: [:show, :update, :create, :destroy]
-      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+      resource :address, only: %i[show update create destroy]
+      resource :address, only: %i[show update create destroy], path: 'relationships/address'
     end
   end
 
