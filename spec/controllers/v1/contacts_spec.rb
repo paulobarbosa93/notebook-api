08683fc6a17967eaef522f41db9e6927ae18911f
Paulo Barbosa <paulo.n.barbosa@hotmail.com>
2018-06-01 12:14:03 -0300

    Rspec (http status code and .rspec)

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -4,11 +4,11 @@ describe V1::ContactsController, type: :controller do
   it "request index and return 200 OK" do
     request.accept = 'application/vnd.api+json'
     get :index
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(:ok)
   end
 
   it "request index and return 406 OK" do
     get :index
-    expect(response).to have_http_status(406)
+    expect(response).to have_http_status(:not_acceptable)
   end
 end
